08c289ed09e0add026df3e3341b52e2bc996ac9d
Steve Aksamit <steveaksamit@gmail.com>
2017-09-19 10:12:48 -0500

    tweets are showing

--- app.js
+++ app.js
@@ -2,34 +2,20 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
-var locals = {
-  title: 'An Example',
-  people: [
-    { name: 'Gandalf' },
-    { name: 'Frodo' },
-    { name: 'Hermione' }
-  ]
-};
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-
+const routes = require('./routes');
 
 app.use(morgan('dev'));
+
+app.use(express.static('public'))
 
 app.set('view engine', 'html')
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function (err, output) {
-  console.log(output);
-});
+// nunjucks.render('index.html', function (err, output) {
+// console.log(output);
+// });
 
-
-app.get('/', function (req, res, next) {
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
-});
-
-app.get('/news', function (req, res, next) {
-  res.send('You got to the news page');
-});
+app.use('/', routes);
 
 app.listen(3000, function () {
   console.log('server is listening on port 3000');
